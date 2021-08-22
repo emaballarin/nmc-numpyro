7478c7e3ce53f1c114476945672128b419e520dc
Emanuele Ballarin <emanuele@ballarin.cc>
2021-08-22 06:18:53 +0200

    Update proposals.py

--- nmc-numpyro/proposals.py
+++ nmc-numpyro/proposals.py
@@ -2,74 +2,74 @@
 # -*- coding: utf-8 -*-
 
 # Imports
-import jax.numpy as np
-import numpyro.distributions as dist
+import jax.numpy as jnp
+import numpyro.distributions as dists
 
 
 def minka_normal(
     rng_key, x, grad, hess, normal_sigma_eigen_epsilon=1e-5, normal_sigma_correction=0.1
 ):
-    ndim = np.ndim(x)
+    ndim = jnp.ndim(x)
     if ndim == 0:
-        inv_hess = np.divide(1, hess)
-        dist_type = dist.Normal
+        inv_hess = jnp.divide(1, hess)
+        dist_type = dists.Normal
     else:
-        inv_hess = np.linalg.inv(hess)
-        dist_type = dist.MultivariateNormal
+        inv_hess = jnp.linalg.inv(hess)
+        dist_type = dists.MultivariateNormal
 
-    loc = np.subtract(x, np.dot(inv_hess, grad))
-    sigma = np.negative(inv_hess)
+    loc = jnp.subtract(x, jnp.dot(inv_hess, grad))
+    sigma = jnp.negative(inv_hess)
 
-    if not ndim == 0 and not np.all(np.linalg.eigvals(sigma) > 0):
-        lamb, vec = np.linalg.eigh(sigma)
-        sigma = vec @ np.diag(np.maximum(lamb, normal_sigma_eigen_epsilon)) @ vec.T
+    if not ndim == 0 and not jnp.all(jnp.linalg.eigvalsh(sigma) > 0):
+        lamb, vec = jnp.linalg.eigh(sigma)
+        sigma = vec @ jnp.diag(jnp.maximum(lamb, normal_sigma_eigen_epsilon)) @ vec.T
 
-    dist_ = dist_type(loc, np.add(sigma, normal_sigma_correction))
+    dist = dist_type(loc, jnp.add(sigma, normal_sigma_correction))
 
-    return dist_.sample(rng_key).reshape(x.shape), dist_
+    return dist.sample(rng_key).reshape(x.shape), dist
 
 
 def minka_gamma(rng_key, x, grad, hess, gamma_concentration_correction=1.3):
-    alpha = np.subtract(1, np.dot(np.power(x, 2), hess))
-    beta = np.subtract(np.negative(np.dot(x, hess)), grad)
+    alpha = jnp.subtract(1, jnp.dot(jnp.power(x, 2), hess))
+    beta = jnp.subtract(jnp.negative(jnp.dot(x, hess)), grad)
 
-    dist_ = dist.continuous.Gamma(
-        concentration=np.multiply(alpha, gamma_concentration_correction), rate=beta
+    dist = dists.continuous.Gamma(
+        concentration=jnp.multiply(alpha, gamma_concentration_correction), rate=beta
     )
 
-    return dist_.sample(rng_key).reshape(x.shape), dist_
+    return dist.sample(rng_key).reshape(x.shape), dist
 
 
 def minka_dirichlet(rng_key, x, grad, hess, dirichlet_concentration_correction=1):
     _ = grad
 
-    max_ndiag_hess = np.max(
-        hess[np.logical_not(np.eye(hess.shape[0], dtype=bool))].reshape(
+    max_ndiag_hess = jnp.max(
+        hess[jnp.logical_not(jnp.eye(hess.shape[0], dtype=bool))].reshape(
             hess.shape[0], -1
         ),
         axis=1,
     )
 
-    concentration = np.subtract(
-        1, np.multiply(np.power(x, 2), np.subtract(np.diag(hess), max_ndiag_hess))
+    concentration = jnp.subtract(
+        1, jnp.multiply(jnp.power(x, 2), jnp.subtract(jnp.diag(hess), max_ndiag_hess))
     )
 
-    dist_ = dist.Dirichlet(
-        concentration=np.add(concentration, dirichlet_concentration_correction)
+    dist = dists.Dirichlet(
+        concentration=jnp.add(concentration, dirichlet_concentration_correction)
     )
 
-    return dist_.sample(rng_key).reshape(x.shape), dist_
+    return dist.sample(rng_key).reshape(x.shape), dist
 
 
-def minka_distribution_match(dist_):
-    support = dist_.support
+def minka_distribution_match(dist):
+    support = dist.support
     if isinstance(
-        support, (dist.constraints._Real, dist.constraints._IndependentConstraint)
+        support, (dists.constraints._Real, dists.constraints._IndependentConstraint)
     ):
         return minka_normal
-    elif isinstance(support, dist.constraints._GreaterThan):
+    elif isinstance(support, dists.constraints._GreaterThan):
         return minka_gamma
-    elif isinstance(support, dist.constraints._Simplex):
+    elif isinstance(support, dists.constraints._Simplex):
         return minka_dirichlet
     else:
         raise RuntimeError(
